3e3d733b1dedfcb294c7344e761b66bc352ce100
Hendrik Kleyn <hkleyn@arcsurge.io>
2026-01-05 21:59:14 +1100

    update app name and icon in GTK

--- hf-gtk/src/window/mod.rs
+++ hf-gtk/src/window/mod.rs
@@ -115,7 +115,7 @@ impl HyperfanWindow {
         let window = adw::ApplicationWindow::builder()
             .application(app)
             .title("Hyperfan")
-            .icon_name("fan-symbolic")
+            .icon_name("io.github.hyperfan")
             .default_width(default_width)
             .default_height(default_height)
             .maximized(was_maximized)
